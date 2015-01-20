560d9c1e03eaaf53ddd6f15b1d998091276a1581
Adam <adam@kakao.com>
2015-01-20 15:24:10 +0900

    first commit

---
+++ test/unit/-position-tracker/positionTrackerSpec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+describe('', function() {
+
+  var module;
+  var dependencies;
+  dependencies = [];
+
+  var hasModule = function(module) {
+  return dependencies.indexOf(module) >= 0;
+  };
+
+  beforeEach(function() {
+
+  // Get module
+  module = angular.module('positionTracker');
+  dependencies = module.requires;
+  });
+
+  it('should load config module', function() {
+    expect(hasModule('positionTracker.config')).to.be.ok;
+  });
+
+  
+
+  
+
+  
+  it('should load services module', function() {
+    expect(hasModule('positionTracker.services')).to.be.ok;
+  });
+  
+
+});
