f8e3087b80f60fe0968c476d3fb03ff6983af015
Adam <adam@kakao.com>
2015-01-23 11:23:20 +0900

    using empty object as strage for test instead of default session storage (refresh storage)

--- test/unit/-position-tracker/positionTrackerSpec.js
+++ test/unit/-position-tracker/positionTrackerSpec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 describe('', function() {
+  var testStorage = {};
   beforeEach(module("positionTracker"));
   beforeEach(module(function($provide){
     var callbacks = [];
@@ -24,26 +25,28 @@ describe('', function() {
   }));
 
   it("should be able to setPosition",inject(function(positionTrackerService){
+    positionTrackerService.config({storage: testStorage});
     positionTrackerService.setPosition({
       x:0,
       y:100,
       url:"first"
     });
-    var savedPosition = (sessionStorage["first_scoll_position"]);
+    var savedPosition = (testStorage["first_scoll_position"]);
     expect(savedPosition).toEqual('{"xPosition":0,"yPosition":100}');
   }))
 
 
   it("should automatically set position", inject(function(positionTrackerService, $rootScope){
+    positionTrackerService.config({storage: testStorage});
     positionTrackerService.
       registerUrlExp(/save_.+/)
 
     $rootScope.$emit("$locationChangeSuccess", {}, 'save_first', 'save_second');
     $rootScope.$emit("$locationChangeSuccess", {}, 'save_second', 'dont_third');
     $rootScope.$emit("$locationChangeSuccess", {}, 'dont_third', 'save_first');
-    var firstPosition = sessionStorage['save_first_scoll_position'];
-    var secondPosition = sessionStorage['save_second_scoll_position'];
-    var thirdPosition = sessionStorage['dont_third_scoll_position'];
+    var firstPosition = testStorage['save_first_scoll_position'];
+    var secondPosition = testStorage['save_second_scoll_position'];
+    var thirdPosition = testStorage['dont_third_scoll_position'];
 
     expect(firstPosition).toEqual('{"yPosition":0}');
     expect(secondPosition).toEqual('{"yPosition":0}');
